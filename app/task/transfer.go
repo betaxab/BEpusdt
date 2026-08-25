6394051fdab89dd185d99e505c8cc73a300562ce
betaxab <betaxab@gmail.com>
2026-08-25 10:35:55 +0800

    fix: Do not perform syncBreak after the order expires

--- app/task/transfer.go
+++ app/task/transfer.go
@@ -301,7 +301,7 @@ func markFinalConfirmed(o model.Order) {
 }
 
 func receivableOrderStatuses() []int {
-	return []int{model.OrderStatusWaiting, model.OrderStatusExpired, model.OrderStatusConfirming}
+	return []int{model.OrderStatusWaiting, model.OrderStatusConfirming}
 }
 
 func getReceivableOrders() (map[string][]model.Order, map[model.TradeType][]model.Order) {
